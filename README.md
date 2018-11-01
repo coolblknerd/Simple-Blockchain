61af0bade61bfb24b08f313f8857fad2987bd5da
Reggie Davis <reginald.davis88@gmail.com>
2018-10-31 23:37:54 -0500

    Added methods for hash and block creation; Add Readme.md

---
+++ README.md
@@ -0,0 +1,6 @@
+# Simple Blockchain
+
+I'm doing this small project to get my golang juices flowing and learn a little bit more about Blockchain. This code is based on the ["Building a Simple Blockchain with Go"](https://www.codementor.io/codehakase/building-a-simple-blockchain-with-go-k7crur06v) article by Francis Sunday on Medium from the CodeMentor blog.
+
+You will see comments along the way and they may not teach you anything new but they are just checkpoints for me to reinforce ideas I'm trying to understand about the language.
+
--- main.go
+++ main.go
@@ -1,35 +1,62 @@
-/* Blockchain: a digital ledger of records in smaller sets called blocks. These blocks are linked with on another through a cryptographic hashing. Each block contains a hash pointing to a previous block. 
+/* Blockchain: a digital ledger of records in smaller sets called blocks. These blocks are linked with on another through a cryptographic hashing. Each block contains a hash pointing to a previous block.
 
 The blockchain is useful for cyptocurrencies because of its decentralize nature, meaning the data stored isn't in a single location, but is accessible by everyone and at same time, immutable by anyone.
 */
 
 package main
 
-import "fmt"
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
+	"time"
+)
 
+/*
+The hashes are used to identify and keep the blocks in the right order.
+*/
 type Block struct {
-	Pos int
-	Data BookCheckout
+	Pos       int
+	Data      BookCheckout
 	Timestamp string
-	Hash string
-	PrevHash string
+	Hash      string
+	PrevHash  string
 }
 
 type BookCheckout struct {
-	BookID string `json:"book_id"`
-	User string `json:"user"`
+	BookID       string `json:"book_id"`
+	User         string `json:"user"`
 	CheckoutDate string `json:"checkout_date"`
-	IsGenesis bool `json:"is_genesis"`
+	IsGenesis    bool   `json:"is_genesis"`
 }
 
 type Book struct {
-	ID string `json:"id"`
-	Title string `json:"title"`
-	Author string `json:"author"`
+	ID          string `json:"id"`
+	Title       string `json:"title"`
+	Author      string `json:"author"`
 	PublishDate string `json:"publsh_date"`
-	ISBN string `json:"isbn"`
+	ISBN        string `json:"isbn"`
 }
 
-func main {
+func (b *Block) generateHash() {
+	bytes, _ := json.Marshal(b.Data)
+	data := string(b.Pos) + b.Timestamp + string(bytes) + b.PrevHash
+	hash := sha256.New()
+	hash.Write([]byte(data))
+	b.Hash = hex.EncodeToString(hash.Sum(nil))
+}
 
+/*
+This method will take a previous block and create a new block based on the info from the previous block.
+*/
+
+func CreateBlock(prevBlock *Block, checkoutItem BookCheckout) *Block {
+	block := &Block{}
+	block.Pos = prevBlock.Pos + 1
+	block.Timestamp = time.Now().String()
+	block.Data = checkoutItem
+	block.PrevHash = prevBlock.Hash
+	block.generateHash()
+
+	return block
 }
